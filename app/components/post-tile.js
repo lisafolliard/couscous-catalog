9f91ebd3e5994f99b62d4826bbff2d7054b9724c
Corrie Smith & Lisa Folliard <student@epicodus.com>
2015-09-23 14:27:00 -0700

    add update method to post

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+
   actions: {
+    update(post,params) {
+      this.sendAction('update', post, params);
+    },
     delete(post) {
       if(confirm("Are you sure you want to see this post go?")) {
         this.sendAction('destroyPost', post);
---
+++ app/components/update-post.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        content: this.get('content'),
+        image: this.get('image'),
+        category: this.get('category'),
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
+});
