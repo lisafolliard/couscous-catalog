df2a9ec0a9f43a0fb6966e930f8c1f5b7e66b1b1
Corrie Smith & Lisa Folliard <student@epicodus.com>
2015-09-23 11:48:58 -0700

    add example posts in database and styling for images

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'couscous-catalog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://couscous-catalog.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
